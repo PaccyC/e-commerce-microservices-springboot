fe0a09c9e8156821154873d55fcb3b57da40dda8
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-04 01:40:03 +0200

    finished customer service

--- services/customer/src/main/java/com/paccy/ecommerce/customer/controller/CustomerController.java
+++ services/customer/src/main/java/com/paccy/ecommerce/customer/controller/CustomerController.java
@@ -1,12 +1,16 @@
 package com.paccy.ecommerce.customer.controller;
 
 
+import com.paccy.ecommerce.customer.Customer;
 import com.paccy.ecommerce.customer.CustomerRequest;
+import com.paccy.ecommerce.customer.CustomerResponse;
 import com.paccy.ecommerce.customer.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customer")
@@ -28,5 +32,32 @@ public class CustomerController {
     ){
         customerService.editCustomer(customerRequest);
         return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping
+    public  List<CustomerResponse> findAllCustomers(){
+        return customerService.findAllCustomers();
+    }
+
+    @GetMapping("/exists/{customer_id}")
+    public Boolean existsById(
+            @PathVariable String customer_id
+    ){
+        return  customerService.existsById(customer_id);
+    }
+
+    @GetMapping("/{customer_id}")
+    public Customer findCustomerById(
+            @PathVariable String customer_id
+    ){
+        return  customerService.findCustomerById(customer_id);
+    }
+
+    @DeleteMapping("/{customer_id}")
+    public  ResponseEntity<Void> deleteCustomer(
+            @PathVariable String customer_id
+    ){
+        customerService.deleteCustomer(customer_id);
+        return  ResponseEntity.accepted().build();
     }
 }
--- services/customer/src/main/java/com/paccy/ecommerce/customer/service/CustomerService.java
+++ services/customer/src/main/java/com/paccy/ecommerce/customer/service/CustomerService.java
@@ -2,12 +2,17 @@ package com.paccy.ecommerce.customer.service;
 
 import com.paccy.ecommerce.customer.Customer;
 import com.paccy.ecommerce.customer.CustomerRequest;
+import com.paccy.ecommerce.customer.CustomerResponse;
 import com.paccy.ecommerce.customer.exceptions.CustomerNotFoundException;
 import com.paccy.ecommerce.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -45,5 +50,29 @@ public class CustomerService {
         if (customerRequest.address() != null){
             customer.setAddress(customerRequest.address());
         }
+    }
+
+    public List<CustomerResponse> findAllCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(mapper::fromCustomer)
+                .collect(Collectors.toList());
+    }
+
+    public Boolean existsById(String customerId) {
+        return  customerRepository.findById(customerId)
+                .isPresent();
+    }
+
+    public Customer findCustomerById(String customerId) {
+        return customerRepository.findById(customerId).orElseThrow(
+                ()-> new CustomerNotFoundException(
+                        String.format("Customer with id '%s' not found", HttpStatus.NOT_FOUND)
+                )
+        );
+    }
+
+    public void deleteCustomer(String customerId) {
+           customerRepository.deleteById(customerId);
     }
 }
