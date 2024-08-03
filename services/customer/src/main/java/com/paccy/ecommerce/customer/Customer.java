2694eee78d28906457f3c15e81eb76c4e4e4086b
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-03 23:44:55 +0200

    configuration of customer service

---
+++ services/customer/src/main/java/com/paccy/ecommerce/customer/Customer.java
@@ -0,0 +1,15 @@
+package com.paccy.ecommerce.customer;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class Customer {
+
+
+}
